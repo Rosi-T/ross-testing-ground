8a9d1debb6df8485b1d6b8cdabcd78f204f3a000
Rosi Terzieva <ro.terzieva@gmail.com>
2021-10-07 22:07:26 +0300

    dark theme front end

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+
+sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
     return src('app/scss/style.scss', { sourcemaps: true })
     .pipe(sass())
-    .pipe(postcss([cssnano()]))
+    .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest('dist', {sourcemaps: '.'}));
 }
 
 // JavaScript Task
 function jsTask() {
     return src('app/js/script.js', {sourcemaps: true})
+    .pipe(babel({presets:['@babel/preset-env']}))
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
